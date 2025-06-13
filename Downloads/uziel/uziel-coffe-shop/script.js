1ed05b64fa34b2b84e03541170f81bf1c8b1a79f
samu <varjao.samueloliveira@gmail.com>
2025-06-13 18:52:03 -0300

    Atualizando arquivos do site

---
+++ Downloads/uziel/uziel-coffe-shop/script.js
@@ -0,0 +1,10 @@
+// Rolagem suave quando clicar nos links do menu
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
